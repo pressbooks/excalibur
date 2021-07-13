81ec574c0e64a32285d1cf5642bb095503bfcb58
SteelWagstaff <steel@pressbooks.com>
2021-07-13 16:22:34 -0700

    Add install-build-tools step. Build assets & lint.

--- assets/scripts/excalibur.js
+++ assets/scripts/excalibur.js
@@ -5,7 +5,7 @@ jQuery( function ( $ ) {
 	} );
 	$( document ).on( 'click', '#pb_contributing_authors .add-row', function ( e ) {
 		e.preventDefault();
-		$( this ).before( '<div class="row"><input type="text" name="pb_contributing_authors[]" value="" class="contributing-author regular-text" /> <button class="button button-small delete-row">Delete Row</button></div>' )
+		$( this ).before( '<div class="row"><input type="text" name="pb_contributing_authors[]" value="" class="contributing-author regular-text" /> <button class="button button-small delete-row">Delete Row</button></div>' );
 	} );
 	$( '#pb_publication_date' ).datepicker( {
 		dateFormat: 'yy-mm-dd',
