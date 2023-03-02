770bceb034536d2c7dfddac80870a0af9d27fd10
Steel Wagstaff <steel@pressbooks.com>
2023-03-01 17:10:40 -0800

    chore: require php 8.1 and update dependencies (#61)

--- excalibur.php
+++ excalibur.php
@@ -5,10 +5,10 @@ Plugin URI: https://github.com/pressbooks/excalibur/
 GitHub Plugin URI: pressbooks/excalibur
 Release Asset: true
 Description: Excalibur is a SWORD protocol client for Pressbooks.
-Version: 0.6.2
+Version: 0.7.0
 Author: Pressbooks (Book Oven Inc.)
 Author URI: https://pressbooks.org
-Requires PHP: 8.0
+Requires PHP: 8.1
 Text Domain: excalibur
 License: GPL v3 or later
 Network: True
